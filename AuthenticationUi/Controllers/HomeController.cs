dddee5ced7d795efeeac44635220b350522a1978
Sehar Ajmal <seharajmal452@gmail.com>
2025-07-28 16:20:43 +0500

    Ui implementation

---
+++ AuthenticationUi/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using AuthenticationUi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace AuthenticationUi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterView model)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginView model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout(RegisterView model)
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AuthenticationUi/Models/LoginView.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationUi.Models
+{
+    public class LoginView
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
